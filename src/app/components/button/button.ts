b0e67c8ac1e31d17244fd6f8a816e272617f7127
dylan9b <dylan9b@gmail.com>
2025-09-24 10:51:41 +0200

    Added button componnet. Updated home component.

---
+++ src/app/components/button/button.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component, HostBinding, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.html',
+  styleUrl: './button.scss',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Button {
+  protected readonly clickButton = output<void>();
+
+  readonly label = input.required<string>();
+  readonly primary = input<boolean>(false);
+  readonly secondary = input<boolean>(false);
+
+  readonly variant = input<'primary' | 'secondary'>('primary');
+
+  @HostBinding('class.primary') get isPrimary() {
+    return this.variant() === 'primary';
+  }
+
+  @HostBinding('class.secondary') get isSecondary() {
+    return this.variant() === 'secondary';
+  }
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,13 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Button } from '@components/button/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Button],
   templateUrl: './home.html',
   styleUrl: './home.scss',
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Home {
-
+  onUploadFile(): void {
+    console.log('upload file');
+  }
 }
