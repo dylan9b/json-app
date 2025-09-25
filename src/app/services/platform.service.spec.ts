c662b0f1f718c691f73db3dbc6e88e5720733c2b
dylan9b <dylan9b@gmail.com>
2025-09-25 13:00:29 +0200

    Removed karma and added jest setup.

--- src/app/services/platform.service.spec.ts
+++ src/app/services/platform.service.spec.ts
@@ -1,15 +1,55 @@
 import { TestBed } from '@angular/core/testing';
 import { PlatformService } from './platform.service';
+import { PLATFORM_ID, provideZonelessChangeDetection } from '@angular/core';
 
 describe('PlatformService', () => {
   let service: PlatformService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PlatformService);
+  describe('when platform is browser', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          { provide: PLATFORM_ID, useValue: 'browser' },
+          provideZonelessChangeDetection(),
+        ],
+      });
+      service = TestBed.inject(PlatformService);
+    });
+
+    it('should detect isBrowser as true', () => {
+      expect(service.isBrowser).toBe(true);
+    });
+
+    it('should detect isServer as false', () => {
+      expect(service.isServer).toBe(false);
+    });
+
+    it('should return localStorage', () => {
+      expect(service.localStorage).toBe(localStorage);
+    });
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('when platform is server', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          { provide: PLATFORM_ID, useValue: 'server' },
+          provideZonelessChangeDetection(),
+        ],
+      });
+      service = TestBed.inject(PlatformService);
+    });
+
+    it('should detect isBrowser as false', () => {
+      expect(service.isBrowser).toBe(false);
+    });
+
+    it('should detect isServer as true', () => {
+      expect(service.isServer).toBe(true);
+    });
+
+    it('should return null for localStorage', () => {
+      expect(service.localStorage).toBeNull();
+    });
   });
 });
