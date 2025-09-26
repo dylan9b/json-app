fc9007f5347ec101f3917bb5ba0c35eee2fbec14
dylan9b <dylan9b@gmail.com>
2025-09-26 16:05:37 +0200

    Fixed lint error and updated page queryParam for list page to default to 1.

--- src/app/pages/files/files.ts
+++ src/app/pages/files/files.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { Button } from '@components/button/button';
 import { SelectModal } from '@components/modal/select-modal/select-modal';
 import { TableRow } from '@components/table-row/table-row';
@@ -17,10 +18,19 @@ import { FileStore } from '@store/files.store';
 export class Files {
   private readonly _modalService = inject(ModalService);
   private readonly _fileStore = inject(FileStore);
+  private readonly _router = inject(Router);
 
   protected readonly filesSignal = this._fileStore.filesUploaded;
 
   onUploadFile(): void {
     this._modalService.open(SelectModal, { size: 'md', centered: true });
+  }
+
+  constructor() {
+    this._router.navigate([], {
+      queryParams: {
+        page: 1,
+      },
+    });
   }
 }
